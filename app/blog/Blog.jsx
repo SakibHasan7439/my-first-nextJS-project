83fac661774da1d42d75be38551e6c4c625df44b
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-01-23 13:24:09 +0600

    page not found issue fixed

--- app/blog/Blog.jsx
+++ app/blog/Blog.jsx
@@ -7,8 +7,8 @@ const Blog = async() => {
         <>
           <ul className="grid grid-cols-12 gap-4">
             {
-              posts.map((post) => <Link className="p-4 hover:bg-blue-500 rounded-md col-span-12 md:col-span-4 lg:col-span-3 shadow-md mb-2"
-                href={`/blog/${post.id}`}>
+              posts.map((post) => <Link key={post.id} className="p-4 transition-colors ease-in-out duration-200 text-center hover:text-white hover:bg-blue-500 rounded-md col-span-12 md:col-span-4 lg:col-span-3 shadow-md mb-2"
+                href={`/Blog/${post.id}`}>
                 <li key={post.id}>
                     <p>{post.title}</p>
                  </li>
