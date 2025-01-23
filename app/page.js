a9fd7475e14443d2be3f43e43b8cba27fe31a4d3
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-01-24 01:17:18 +0600

    file skip

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import Blog from "./Blog/components/Blog";
+// import Blog from "./Blog/components/Blog";
 
 export default function Home() {
 
   return (
     <main className="max-w-7xl w-full mx-auto my-8">
-      <Blog></Blog>
+      {/* <Blog></Blog> */}
     </main>
   );
 }
